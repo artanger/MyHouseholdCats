c24240166adfcf71031e53b9667130d969dce829
artanger <artimag666@gmail.com>
2021-04-09 18:44:28 +0300

    Add project files.

---
+++ MyHouseholdCats/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyHouseholdCats.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
